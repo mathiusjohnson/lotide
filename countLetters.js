435fd84af24634270fda28e1818bac5fcff30f74
Mathius Johnson <devachievem@gmail.com>
2020-07-22 13:34:19 -0700

    countLetters

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😁😁😁Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡😡😡Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = (allItems, itemsToCount) => {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+      console.log(results[item]);
+    }
+  }
+  return results;
+};
+
+const string = "LHL";
+const resultOne = countLetters(string, { "L": true });
+
+assertEqual(resultOne["L"], 2);
