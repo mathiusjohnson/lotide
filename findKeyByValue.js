b69604e53da5bbe0d04c6b81091583b9fce654f0
Mathius Johnson <devachievem@gmail.com>
2020-07-22 15:17:35 -0700

    findKey

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,7 +1,27 @@
+const findKeyByValue = (object, value) => {
+  for (const prop in object) {
+    if (object.hasOwnProperty(prop)) {
+      if (object[prop] === value) {
+        return prop;
+      }
+    }
+  }
+};
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
+    console.log("this is actual:", actual);
     console.log(`😁😁😁Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`😡😡😡Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
