b33978c3dc27ce10a703e3f73929c641bd4b5b4f
Mathius Johnson <devachievem@gmail.com>
2020-07-27 09:21:30 -0700

    tail refactored and added to test code

--- tail.js
+++ tail.js
@@ -1,8 +1,6 @@
 const tail = function(array) {
   let arrayed = array.slice(1);
-  for (let i = 0; i < arrayed.length; i++) {
-    console.log(arrayed);
-  }
+  return arrayed;
 
 };
 
@@ -14,12 +12,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+module.exports = (tail);
